dd1cadc0eebd13421eefe06821cc4c48eeeefc46
AlejandroRuizAlfas <aruizalfas@gmail.com>
2021-10-05 18:03:13 +0200

    [MOD] Ej2 funciona

--- src/Ejercicio2/Ejercicio2.java
+++ src/Ejercicio2/Ejercicio2.java
@@ -1,8 +1,36 @@
 package Ejercicio2;
 
-import java.util.Random;
-import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.*;
 
 public class Ejercicio2 {
+    public static void main(String[] args) {
+        String commands = "java -jar out\\artifacts\\Random10_jar\\U1Practica2.jar";
+        List<String> argsList = new ArrayList<>(Arrays.asList(commands.split(" ")));
+        ProcessBuilder pb = new ProcessBuilder(argsList);
 
+        try {
+            Process process = pb.start();
+            OutputStream os = process.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(os);
+            BufferedWriter bw = new BufferedWriter(osw);
+
+            Scanner precesoSC = new Scanner(process.getInputStream());
+
+            Scanner sc = new Scanner(System.in);
+            String linea = sc.nextLine();
+            while (!linea.equals("stop")){
+                bw.write(linea);
+                bw.newLine();
+                bw.flush();
+                System.out.println(precesoSC.nextLine());
+                linea = sc.nextLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
